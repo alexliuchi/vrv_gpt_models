16132155da5f0913e386ffbc30c2e49bd9f43389
alexliuchi <32992974+alexliuchi@users.noreply.github.com>
2024-11-28 13:17:54 +0800

    Create GPT_CONFIG_1558M.py

---
+++ configs/GPT_CONFIG_1558M.py
@@ -0,0 +1,16 @@
+'''
+vrv
+@author: Alex Liu
+@description:
+@date:
+'''
+
+GPT_CONFIG_1558M = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "emb_dim": 1600,         # Embedding dimension
+    "n_heads": 25,           # Number of attention heads
+    "n_layers": 48,          # Number of layers
+    "drop_rate": 0.1,        # Dropout rate
+    "qkv_bias": False        # Query-Key-Value bias
+}
